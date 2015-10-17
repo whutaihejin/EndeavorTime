fd0db90c22727fd80da99b8df54fb5ae927b6092
zhanglong <zhanglong@staff.weibo.com>
2015-10-17 15:00:18 +0800

    std::pair study

---
+++ primer/chapter10/content101.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+
+int main(int argc, char** argv) {
+  std::pair<std::string, std::string> anon;
+  std::pair<std::string, int> word_count;
+  std::pair<std::string, std::vector<int> > line;
+  return 0;
+}
