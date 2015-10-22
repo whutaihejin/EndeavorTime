d003df1c49e8199c944d79be1e5ec67e8e1216dd
taihejin <taihejin@163.com>
2015-10-22 16:34:46 +0800

    done problem of 1021

--- jobdu/e1021.cc
+++ jobdu/e1021.cc
@@ -1,5 +1,37 @@
 #include <iostream>
 
+int index(std::string& key, char c) {
+  for (int i = 0; i < key.size(); i++) {
+    if (key[i] == c) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 int main(int argc, char** argv) {
+  std::string key;
+  while (getline(std::cin, key)) {
+    if (key == "#") {
+      return 0;
+    }
+    // value input
+    std::string value;
+    getline(std::cin, value);
+
+    // statistic
+    int count[5] = {0};
+    char *p = &value[0], *limit = p + value.size();
+    for (; p < limit; ++p) {
+      int idx = index(key, *p);
+      if (idx != -1) {
+        count[idx]++;
+      } 
+    }
+    // output
+    for (int i = 0; i < key.size(); i++) {
+      std::cout << key[i] << " " << count[i] << std::endl;
+    }
+  }
   return 0;
 }
