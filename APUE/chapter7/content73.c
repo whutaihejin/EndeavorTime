622d3ae29f230705541d4e23c80f2e9ebd2c4d77
whutaihejin <taihejin@163.com>
2015-11-08 11:58:10 +0800

    learn forever

---
+++ APUE/chapter7/content73.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static void my_exit1(void);
+static void my_exit2(void);
+
+int main(int argc, char *argv[]) {
+	if (atexit(my_exit2) != 0) {
+		printf("can't register my_exit2");
+	}
+	
+	if (atexit(my_exit1) != 0) {
+		printf("can't register my_exit1");
+	}
+	
+	if (atexit(my_exit1) != 0) {
+		printf("can't register my_exit1");
+	}
+	printf("main is done\n");
+	return 0;
+}
+
+static void my_exit1(void) {
+	printf("first exit handler\n");
+}
+
+static void my_exit2(void) {
+	printf("second exit handler\n");
+}
