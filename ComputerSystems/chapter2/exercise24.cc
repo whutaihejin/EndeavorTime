e7a6c8427988289d8902ed93de54b3c3be4399b6
zhanglong <zhanglong@staff.weibo.com>
2015-11-13 12:12:02 +0800

    remove old version

--- ComputerSystems/chapter2/exercise24.cc
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(int argc, char** argv) {
-	printf("%x\n", 0x503c + 0x8);
-	printf("%x\n", 0x503c - 0x40);
-	printf("%x\n", 0x503c + 64);
-	printf("%x\n", 0x50ea - 0x503c);
-	return 0;
-}
