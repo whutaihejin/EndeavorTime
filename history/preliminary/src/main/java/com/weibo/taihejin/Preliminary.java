589e88aab158ecd4e09a8edef549babdc3cc4b31
zhanglong <zhanglong@staff.weibo.com>
2015-11-15 21:04:42 +0800

    first hadoop program: world count

---
+++ history/preliminary/src/main/java/com/weibo/taihejin/Preliminary.java
@@ -0,0 +1,15 @@
+package com.weibo.taihejin;
+
+/**
+ * Created by taihejin on 15-11-15.
+ */
+public class Preliminary {
+
+    public String sayHello(String name) {
+        return "hello " + name;
+    }
+
+    public static void main(String[] args) {
+        new Preliminary().sayHello("taihejin");
+    }
+}
---
+++ history/preliminary/src/test/java/com/weibo/taihejin/PreliminaryTest.java
@@ -0,0 +1,16 @@
+package com.weibo.taihejin;
+
+import  com.weibo.taihejin.Preliminary;
+import org.junit.Test;
+import org.junit.Assert;
+/**
+ * Created by taihejin on 15-11-15.
+ */
+public class PreliminaryTest {
+    @Test
+    public void testSayHello() {
+        Preliminary preliminary = new Preliminary();
+        String result = preliminary.sayHello("taihejin");
+        Assert.assertEquals(result, "hello taihejin");
+    }
+}
